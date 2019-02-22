70abb61c9536234af871141082771059ef393895
Youri Seichter, Instance <yseichter@gmail.com>
2019-02-22 15:55:43 +0000

    Updates

---
+++ precache-manifest.c7ff1b20b1afe0d3fe28912e314da23d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99cca734b255bc5e44c1",
+    "url": "/playfind/static/js/runtime~main.99cca734.js"
+  },
+  {
+    "revision": "54f221ef64f3f335e293",
+    "url": "/playfind/static/js/main.54f221ef.chunk.js"
+  },
+  {
+    "revision": "3da4f0c0ee99217e51da",
+    "url": "/playfind/static/js/2.3da4f0c0.chunk.js"
+  },
+  {
+    "revision": "54f221ef64f3f335e293",
+    "url": "/playfind/static/css/main.8045d202.chunk.css"
+  },
+  {
+    "revision": "f7d78aeeff3244e5419b872e0960540a",
+    "url": "/playfind/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/playfind/precache-manifest.c7ff1b20b1afe0d3fe28912e314da23d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/playfind/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
